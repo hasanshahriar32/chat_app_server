eb957dec84cfde94ddddb0adc83bbb5916d69257
Mr. Hasan <hasanshahriar32@gmail.com>
2023-03-25 02:55:50 +0600

    pic model updated

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -6,7 +6,7 @@ const userSchema = mongoose.Schema(
     name: { type: String, trim: true, required: true },
     email: { type: String, trim: true, required: true, unique: true },
     password: { type: String, trim: true, required: true },
-    profilePic: {
+    pic: {
       type: String,
       default:
         "https://e1.pngegg.com/pngimages/444/382/png-clipart-frost-pro-for-os-x-icon-set-now-free-contacts-male-profile.png",
