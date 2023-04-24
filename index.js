7f41b192a82b36f144776e71975621f3d82836c9
Mr. Hasan <hasanshahriar32@gmail.com>
2023-04-24 08:30:54 +0600

    updated cors policy

--- index.js
+++ index.js
@@ -48,11 +48,7 @@ const server = app.listen(PORT, () => {
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: [
-      "http://localhost:3000",
-      "https://chatfriend.vercel.app",
-      "https://*.vercel.app",
-    ],
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   },
